7b3c2b4fd67805263e40fc5b9948fa9d111ccbf1
RedikultsevEvg <effect-project@yandex.ru>
2018-09-24 22:19:40 +0500

    Добавил конвертеры

---
+++ UIL/Converters/MultThousText.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RDUIL.Converters
+{
+    public class MultThousText : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return "к" + (String)value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+
+            return value;
+        }
+    }
+}
