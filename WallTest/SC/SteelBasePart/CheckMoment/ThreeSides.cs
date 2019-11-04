e7a09ef39e1c57e83fa257bea693bdf708ed11d4
Evgeny Redikultsev <effect-project@yandex.ru>
2019-11-04 13:31:42 +0500

    Исправил тесты по расчету участков базы стальной колонны

---
+++ WallTest/SC/SteelBasePart/CheckMoment/ThreeSides.cs
@@ -0,0 +1,263 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RDBLL.Entity.SC.Column;
+using RDBLL.Forces;
+using RDBLL.Processors.SC;
+
+namespace Test.SC.SteelBasePart.CheckMoment
+{
+
+    [TestClass]
+    public class ThreeSides
+    {
+        #region ThreeSides1
+        [TestMethod]
+        //Три стороны свободная сверху
+        public void CheckMomentPartThreeSides1()
+        {
+            double force = -10000000;
+            double baseWidth = 1;
+            double baseLength = 1;
+            double width = 0.2;
+            double length = 0.3;
+            double maxStress = force * (-1D);
+
+            SteelBase steelColumnBase = new SteelBase();
+            steelColumnBase.Width = baseWidth;
+            steelColumnBase.Length = baseLength;
+            steelColumnBase.Thickness = 0.05;
+
+            LoadSet loadSet = steelColumnBase.LoadsGroup[0].LoadSets[0];
+            ForceParameter forceParameter = new ForceParameter();
+            loadSet.ForceParameters.Add(forceParameter);
+            forceParameter.KindId = 1;
+            forceParameter.CrcValue = force;
+            loadSet.PartialSafetyFactor = 1;
+
+            RDBLL.Entity.SC.Column.SteelBasePart basePart = new RDBLL.Entity.SC.Column.SteelBasePart(steelColumnBase);
+            basePart.Width = width;
+            basePart.Length = length;
+            basePart.CenterX = 0;
+            basePart.CenterY = 0;
+            basePart.FixLeft = true;
+            basePart.FixRight = true;
+            basePart.FixTop = false;
+            basePart.FixBottom = true;
+            SteelBaseProcessor.ActualizeLoadCases(steelColumnBase);
+
+            double Actual = SteelBasePartProcessor.GetResult(basePart, maxStress)[0];
+            double Expected = 50800;
+
+            Assert.AreEqual(Actual, Expected, Expected / 1000);
+        }
+        #endregion
+        #region ThreeSides1a
+        [TestMethod]
+        //Три стороны свободная справа
+        public void CheckMomentPartThreeSides1a()
+        {
+            double force = -10000000;
+            double baseWidth = 1;
+            double baseLength = 1;
+            double width = 0.3;
+            double length = 0.2;
+            double maxStress = force * (-1D);
+
+            SteelBase steelColumnBase = new SteelBase();
+            steelColumnBase.Width = baseWidth;
+            steelColumnBase.Length = baseLength;
+            steelColumnBase.Thickness = 0.05;
+
+            LoadSet loadSet = steelColumnBase.LoadsGroup[0].LoadSets[0];
+            ForceParameter forceParameter = new ForceParameter();
+            loadSet.ForceParameters.Add(forceParameter);
+            forceParameter.KindId = 1;
+            forceParameter.CrcValue = force;
+            loadSet.PartialSafetyFactor = 1;
+
+            RDBLL.Entity.SC.Column.SteelBasePart basePart = new RDBLL.Entity.SC.Column.SteelBasePart(steelColumnBase);
+            basePart.Width = width;
+            basePart.Length = length;
+            basePart.CenterX = 0;
+            basePart.CenterY = 0;
+            basePart.FixLeft = true;
+            basePart.FixRight = false;
+            basePart.FixTop = true;
+            basePart.FixBottom = true;
+            SteelBaseProcessor.ActualizeLoadCases(steelColumnBase);
+
+            double Actual = SteelBasePartProcessor.GetResult(basePart, maxStress)[0];
+            double Expected = 50800;
+
+            Assert.AreEqual(Actual, Expected, Expected / 1000);
+        }
+        #endregion
+        #region ThreeSides2
+        [TestMethod]
+        //Три стороны свободная сверху с большим соотношением сторон,
+        //таким образом, чтобы считалось по шарнирной схеме
+        public void CheckMomentPartThreeSides2()
+        {
+            double force = -10000000;
+            double baseWidth = 1;
+            double baseLength = 1;
+            double width = 0.2;
+            double length = 0.5;
+            double maxStress = force * (-1D);
+
+            SteelBase steelColumnBase = new SteelBase();
+            steelColumnBase.Width = baseWidth;
+            steelColumnBase.Length = baseLength;
+            steelColumnBase.Thickness = 0.05;
+
+            LoadSet loadSet = steelColumnBase.LoadsGroup[0].LoadSets[0];
+            ForceParameter forceParameter = new ForceParameter();
+            loadSet.ForceParameters.Add(forceParameter);
+            forceParameter.KindId = 1;
+            forceParameter.CrcValue = force;
+            loadSet.PartialSafetyFactor = 1;
+
+            RDBLL.Entity.SC.Column.SteelBasePart basePart = new RDBLL.Entity.SC.Column.SteelBasePart(steelColumnBase);
+            basePart.Width = width;
+            basePart.Length = length;
+            basePart.CenterX = 0;
+            basePart.CenterY = 0;
+            basePart.FixLeft = true;
+            basePart.FixRight = true;
+            basePart.FixTop = false;
+            basePart.FixBottom = true;
+            SteelBaseProcessor.ActualizeLoadCases(steelColumnBase);
+
+            double Actual = SteelBasePartProcessor.GetResult(basePart, maxStress)[0];
+            double Expected = (maxStress / 1 / 1) * width * width / 8;
+
+            Assert.AreEqual(Expected, Actual, Expected / 1000);
+        }
+        #endregion
+        #region ThreeSides2a
+        [TestMethod]
+        //Три стороны свободная справа с большим соотношением сторон,
+        //таким образом, чтобы считалось по шарнирной схеме
+        public void CheckMomentPartThreeSides2a()
+        {
+            double force = -10000000;
+            double baseWidth = 1;
+            double baseLength = 1;
+            double width = 0.5;
+            double length = 0.2;
+            double maxStress = force * (-1D);
+
+            SteelBase steelColumnBase = new SteelBase();
+            steelColumnBase.Width = baseWidth;
+            steelColumnBase.Length = baseLength;
+            steelColumnBase.Thickness = 0.05;
+
+            LoadSet loadSet = steelColumnBase.LoadsGroup[0].LoadSets[0];
+            ForceParameter forceParameter = new ForceParameter();
+            loadSet.ForceParameters.Add(forceParameter);
+            forceParameter.KindId = 1;
+            forceParameter.CrcValue = force;
+            loadSet.PartialSafetyFactor = 1;
+
+            RDBLL.Entity.SC.Column.SteelBasePart basePart = new RDBLL.Entity.SC.Column.SteelBasePart(steelColumnBase);
+            basePart.Width = width;
+            basePart.Length = length;
+            basePart.CenterX = 0;
+            basePart.CenterY = 0;
+            basePart.FixLeft = true;
+            basePart.FixRight = false;
+            basePart.FixTop = true;
+            basePart.FixBottom = true;
+            SteelBaseProcessor.ActualizeLoadCases(steelColumnBase);
+
+            double Actual = SteelBasePartProcessor.GetResult(basePart, maxStress)[0];
+            double Expected = (maxStress / 1 / 1) * length * length / 8;
+
+            Assert.AreEqual(Expected, Actual, Expected / 1000);
+        }
+        #endregion
+        #region ThreeSides3
+        [TestMethod]
+        //Три стороны свободная сверху с большим соотношением сторон,
+        //таким образом, чтобы считалось по консольной схеме
+        public void CheckMomentPartThreeSides3()
+        {
+            double force = -10000000;
+            double baseWidth = 1;
+            double baseLength = 1;
+            double width = 0.5;
+            double length = 0.2;
+            double maxStress = force * (-1D);
+
+            SteelBase steelColumnBase = new SteelBase();
+            steelColumnBase.Width = baseWidth;
+            steelColumnBase.Length = baseLength;
+            steelColumnBase.Thickness = 0.05;
+
+            LoadSet loadSet = steelColumnBase.LoadsGroup[0].LoadSets[0];
+            ForceParameter forceParameter = new ForceParameter();
+            loadSet.ForceParameters.Add(forceParameter);
+            forceParameter.KindId = 1;
+            forceParameter.CrcValue = force;
+            loadSet.PartialSafetyFactor = 1;
+
+            RDBLL.Entity.SC.Column.SteelBasePart basePart = new RDBLL.Entity.SC.Column.SteelBasePart(steelColumnBase);
+            basePart.Width = width;
+            basePart.Length = length;
+            basePart.CenterX = 0;
+            basePart.CenterY = 0;
+            basePart.FixLeft = true;
+            basePart.FixRight = true;
+            basePart.FixTop = false;
+            basePart.FixBottom = true;
+            SteelBaseProcessor.ActualizeLoadCases(steelColumnBase);
+
+            double Actual = SteelBasePartProcessor.GetResult(basePart, maxStress)[0];
+            double Expected = (maxStress / 1 / 1) * length * length / 2;
+
+            Assert.AreEqual(Expected, Actual, Expected / 1000);
+        }
+        #endregion
+        #region ThreeSides3a
+        [TestMethod]
+        //Три стороны свободная справа с большим соотношением сторон,
+        //таким образом, чтобы считалось по консольной схеме
+        public void CheckMomentPartThreeSides3a()
+        {
+            double force = -10000000;
+            double baseWidth = 1;
+            double baseLength = 1;
+            double width = 0.2;
+            double length = 0.5;
+            double maxStress = force * (-1D);
+
+            SteelBase steelColumnBase = new SteelBase();
+            steelColumnBase.Width = baseWidth;
+            steelColumnBase.Length = baseLength;
+            steelColumnBase.Thickness = 0.05;
+
+            LoadSet loadSet = steelColumnBase.LoadsGroup[0].LoadSets[0];
+            ForceParameter forceParameter = new ForceParameter();
+            loadSet.ForceParameters.Add(forceParameter);
+            forceParameter.KindId = 1;
+            forceParameter.CrcValue = force;
+            loadSet.PartialSafetyFactor = 1;
+
+            RDBLL.Entity.SC.Column.SteelBasePart basePart = new RDBLL.Entity.SC.Column.SteelBasePart(steelColumnBase);
+            basePart.Width = width;
+            basePart.Length = length;
+            basePart.CenterX = 0;
+            basePart.CenterY = 0;
+            basePart.FixLeft = true;
+            basePart.FixRight = false;
+            basePart.FixTop = true;
+            basePart.FixBottom = true;
+            SteelBaseProcessor.ActualizeLoadCases(steelColumnBase);
+
+            double Actual = SteelBasePartProcessor.GetResult(basePart, maxStress)[0];
+            double Expected = (maxStress / 1 / 1) * width * width / 2;
+
+            Assert.AreEqual(Expected, Actual, Expected / 1000);
+        }
+        #endregion
+    }
+}
--- WallTest/WallTest.cs
+++ WallTest/WallTest.cs
@@ -22,220 +22,31 @@ namespace WallTest
             Assert.AreEqual(0.112, inter2number);
         }
 
-        [TestMethod, Timeout(300)]
-        
-        ///
-        public void CheckSteelBasePart()
-        {
-            SteelBase steelColumnBase = new SteelBase();
-            steelColumnBase.Width = 1;
-            steelColumnBase.Length = 1;
-            steelColumnBase.Thickness = 0.05;
-            LoadSet loadSet = steelColumnBase.LoadsGroup[0].LoadSets[0];
-            ForceParameter forceParameter = new ForceParameter();
-            loadSet.ForceParameters.Add(forceParameter);
-            forceParameter.KindId = 1;
-            forceParameter.CrcValue = -100000;
-            loadSet.PartialSafetyFactor = 1;
-            SteelBasePart basePart = new SteelBasePart(steelColumnBase);
-            basePart.FixLeft = true;
-            basePart.FixRight = true;
-            basePart.FixTop = false;
-            basePart.FixBottom = false;
-            basePart.Width = 1;
-            basePart.Length = 1;
-            SteelBaseProcessor.ActualizeLoadCases(steelColumnBase);
+        //[TestMethod, Timeout(300)]        
+        ////
+        //public void CheckSteelBasePart()
+        //{
+        //    SteelBase steelColumnBase = new SteelBase();
+        //    steelColumnBase.Width = 1;
+        //    steelColumnBase.Length = 1;
+        //    steelColumnBase.Thickness = 0.05;
+        //    LoadSet loadSet = steelColumnBase.LoadsGroup[0].LoadSets[0];
+        //    ForceParameter forceParameter = new ForceParameter();
+        //    loadSet.ForceParameters.Add(forceParameter);
+        //    forceParameter.KindId = 1;
+        //    forceParameter.CrcValue = -100000;
+        //    loadSet.PartialSafetyFactor = 1;
+        //    SteelBasePart basePart = new SteelBasePart(steelColumnBase);
+        //    basePart.FixLeft = true;
+        //    basePart.FixRight = true;
+        //    basePart.FixTop = false;
+        //    basePart.FixBottom = false;
+        //    basePart.Width = 1;
+        //    basePart.Length = 1;
+        //    SteelBaseProcessor.ActualizeLoadCases(steelColumnBase);
 
-            double Actual = SteelBasePartProcessor.GetResult(basePart)[1];
-            Assert.AreEqual(30, Actual / 1000000, 10);
-        }
-
-        [TestMethod, Timeout(300)]
-        ///Проверка работоспособности инстумента создания комбинации нагрузок
-        /// 
-        public void CheckLoadCaseOnlyDeadLoad()
-        {
-            SteelBase steelColumnBase = new SteelBase();
-            int i = 1;
-            AddForceParameter(steelColumnBase.LoadsGroup[0], i, false);
-            
-            i = 2;
-            AddForceParameter(steelColumnBase.LoadsGroup[0], i, false);
-
-            List<LoadSet> ExpLoadCases = new List<LoadSet>();
-
-            List<LoadSet> ActualList = LoadSetProcessor.GetLoadCases(steelColumnBase.LoadsGroup);
-            List<LoadSet> ExpectedList = ExpLoadCases;
-            #region //Свойства ожидаемой комбинации нагрузок
-            ExpectedList.Add(new LoadSet());
-            LoadSet expLoadSet = new LoadSet();
-            ExpectedList[0] = expLoadSet;
-            expLoadSet.Name = "Новая нагрузка*(1) + New_load_1*(1) + New_load_2*(1)";
-            expLoadSet.ForceParameters.Add(new ForceParameter());
-            expLoadSet.ForceParameters[0].KindId = 1; //Продольная сила
-            expLoadSet.ForceParameters[0].CrcValue = -200000; //Продольная сила
-            expLoadSet.ForceParameters[0].DesignValue = -220000; //Продольная сила
-            expLoadSet.IsLiveLoad = false;
-            expLoadSet.PartialSafetyFactor = 1.1;
-            #endregion
-
-            bool actual = CompareLoadList(ActualList, ExpectedList);
-            bool expected = true;
-
-            Assert.AreEqual(actual, expected);
-        }
-
-        [TestMethod, Timeout(300)]
-        ///Проверка работоспособности инстумента создания комбинации нагрузок
-        /// 
-        public void CheckLoadCaseLiveLoad()
-        {
-            SteelBase steelColumnBase = new SteelBase();
-            int i = 1;
-            AddForceParameter(steelColumnBase.LoadsGroup[0], i, false);
-
-            i = 2;
-            AddForceParameter(steelColumnBase.LoadsGroup[0], i, true);
-
-            List<LoadSet> ExpLoadCases = new List<LoadSet>();
-
-            List<LoadSet> ActualList = LoadSetProcessor.GetLoadCases(steelColumnBase.LoadsGroup);
-            List<LoadSet> ExpectedList = ExpLoadCases;
-            #region //Свойства ожидаемой комбинации нагрузок
-            ExpectedList.Add(new LoadSet());
-            LoadSet expLoadSet = new LoadSet();
-            ExpectedList[0] = expLoadSet;
-            expLoadSet.Name = "Новая нагрузка*(1) + New_load_1*(1)";
-            expLoadSet.ForceParameters.Add(new ForceParameter());
-            expLoadSet.ForceParameters[0].KindId = 1; //Продольная сила
-            expLoadSet.ForceParameters[0].CrcValue = -100000; //Продольная сила
-            expLoadSet.ForceParameters[0].DesignValue = -110000; //Продольная сила
-            expLoadSet.IsLiveLoad = false;
-            expLoadSet.PartialSafetyFactor = 1.1;
-
-
-            ExpectedList.Add(new LoadSet());
-            LoadSet expLoadSet1 = new LoadSet();
-            ExpectedList[1] = expLoadSet1;
-            expLoadSet1.Name = "Новая нагрузка*(1) + New_load_1*(1) + New_load_2*(1)";
-            expLoadSet1.ForceParameters.Add(new ForceParameter());
-            expLoadSet1.ForceParameters[0].KindId = 1; //Продольная сила
-            expLoadSet1.ForceParameters[0].CrcValue = -200000; //Продольная сила
-            expLoadSet1.ForceParameters[0].DesignValue = -220000; //Продольная сила
-            expLoadSet1.IsLiveLoad = false;
-            expLoadSet1.PartialSafetyFactor = 1.1;
-            #endregion
-
-            bool actual = CompareLoadList(ActualList, ExpectedList);
-            bool expected = true;
-
-            Assert.AreEqual(actual, expected);
-            Assert.AreEqual(ExpectedList[1].ForceParameters[0].CrcValue, ActualList[1].ForceParameters[0].CrcValue);
-        }
-
-        [TestMethod, Timeout(300)]
-        ///Проверка работоспособности инстумента создания комбинации нагрузок
-        /// 
-        public void CheckLoadCaseLiveChangableLoad()
-        {
-            SteelBase steelColumnBase = new SteelBase();
-            int i = 1;
-            AddForceParameter(steelColumnBase.LoadsGroup[0], i);
-
-            i = 2;
-            AddForceParameter(steelColumnBase.LoadsGroup[0], i, true, true);
-
-            List<LoadSet> ExpLoadCases = new List<LoadSet>();
-
-            List<LoadSet> ActualList = LoadSetProcessor.GetLoadCases(steelColumnBase.LoadsGroup);
-            List<LoadSet> ExpectedList = ExpLoadCases;
-            #region //Свойства ожидаемой комбинации нагрузок
-            ExpectedList.Add(new LoadSet());
-            LoadSet expLoadSet = new LoadSet();
-            ExpectedList[0] = expLoadSet;
-            expLoadSet.Name = "Новая нагрузка*(1) + New_load_1*(1)";
-            expLoadSet.ForceParameters.Add(new ForceParameter());
-            expLoadSet.ForceParameters[0].KindId = 1; //Продольная сила
-            expLoadSet.ForceParameters[0].CrcValue = -100000; //Продольная сила
-            expLoadSet.ForceParameters[0].DesignValue = -110000; //Продольная сила
-
-            expLoadSet.IsLiveLoad = false;
-            expLoadSet.PartialSafetyFactor = 1.1;
-            
-            ExpectedList.Add(new LoadSet());
-            LoadSet expLoadSet1 = new LoadSet();
-            ExpectedList[1] = expLoadSet1;
-            expLoadSet1.Name = "Новая нагрузка*(1) + New_load_1*(1) + New_load_2*(1)";
-            expLoadSet1.ForceParameters.Add(new ForceParameter());
-            expLoadSet1.ForceParameters[0].KindId = 1; //Продольная сила
-            expLoadSet1.ForceParameters[0].CrcValue = -200000; //Продольная сила
-            expLoadSet1.ForceParameters[0].DesignValue = -220000; //Продольная сила
-
-            expLoadSet1.IsLiveLoad = true;
-            expLoadSet1.PartialSafetyFactor = 1.1;
-
-            ExpectedList.Add(new LoadSet());
-            LoadSet expLoadSet2 = new LoadSet();
-            ExpectedList[2] = expLoadSet2;
-            expLoadSet2.Name = "Новая нагрузка*(1) + New_load_1*(1) + New_load_2*(-1)";
-            expLoadSet2.ForceParameters.Add(new ForceParameter());
-            expLoadSet2.ForceParameters[0].KindId = 1; //Продольная сила
-            expLoadSet2.ForceParameters[0].CrcValue = 0; //Продольная сила
-            expLoadSet2.ForceParameters[0].DesignValue = 0; //Продольная сила
-            expLoadSet2.IsLiveLoad = true;
-            expLoadSet2.PartialSafetyFactor = 1.1;
-            #endregion
-
-            bool actual = CompareLoadList(ActualList, ExpectedList);
-            bool expected = true;
-
-            Assert.AreEqual(actual, expected);
-        }
-
-        /// <summary>
-        /// Добавляет нагрузку в группу нагрузок
-        /// </summary>
-        /// <param name="forcesGroup"></param>
-        /// <param name="i">Порядковый номер нагрузки</param>
-        /// <param name="isLiveLoad">Флаг временной нагрузки</param>
-        /// /// <param name="bothSign">Флаг знакопеременной нагрузки</param>
-        public static void AddForceParameter (ForcesGroup forcesGroup, int i, bool isLiveLoad=false, bool bothSign=false)
-        {
-            LoadSet loadSet = new LoadSet();
-            forcesGroup.LoadSets.Add(loadSet);
-            loadSet.Name = $"New_load_{i}";
-            loadSet.ForceParameters.Add(new ForceParameter());
-            loadSet.ForceParameters[0].KindId = 1; //Продольная сила
-            loadSet.ForceParameters[0].CrcValue = -100000; //Продольная сила
-            loadSet.IsLiveLoad = isLiveLoad;
-            loadSet.BothSign = bothSign;
-            loadSet.PartialSafetyFactor = 1.1;
-        }
-
-        /// <summary>
-        /// Сравнивает два списка набора нагрузок
-        /// </summary>
-        /// <param name="frstList"></param>
-        /// <param name="scndtList"></param>
-        /// <returns></returns>
-        public static bool CompareBarLoadList(List<BarLoadSet> frstList, List<BarLoadSet> scndtList)
-        {
-            if (!(scndtList.Count == frstList.Count)) { return false; } //Если количество не совпадает нет смысла сравнивать
-            for (int i = 0; i < frstList.Count; i++)
-            {
-                if (! frstList[i].Equals(scndtList[i])) { return false; }
-            }
-            return true;
-        }
-
-        public static bool CompareLoadList(List<LoadSet> frstList, List<LoadSet> scndtList)
-        {
-            if (!(scndtList.Count == frstList.Count)) { return false; } //Если количество не совпадает нет смысла сравнивать
-            for (int i = 0; i < frstList.Count; i++)
-            {
-                if (!frstList[i].Equals(scndtList[i])) { return false; }
-            }
-            return true;
-        }
+        //    double Actual = SteelBasePartProcessor.GetResult(basePart)[1];
+        //    Assert.AreEqual(30, Actual / 1000000, 10);
+        //}
     }
 }
