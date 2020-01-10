b103caa5215efa591968b7536613616f1fd0f3b2
Evgeny Redikultsev <effect-project@yandex.ru>
2020-01-10 17:32:10 +0500

    Добавил классы грунтов

---
+++ BLL/Entity/Soil/RockSoil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDBLL.Entity.Soil
+{
+    /// <summary>
+    /// Класс скального грунта
+    /// </summary>
+    public class RockSoil :Soil
+    {       
+        public override double[] GetSoilResistance()
+        {
+            double[] resistance = new double[2];
+            return resistance;
+        }
+    }
+}
