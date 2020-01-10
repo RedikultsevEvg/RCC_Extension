d45f308bc7ddb932ae8f33ae07bb6f7ed0f0b600
Evgeny Redikultsev <effect-project@yandex.ru>
2020-01-10 17:32:10 +0500

    Добавил классы грунтов

---
+++ BLL/Entity/Soil/Soil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDBLL.Entity.Soil
+{
+    /// <summary>
+    /// Абстрактный класс грунта
+    /// </summary>
+    public abstract class Soil
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double ElasticModulus { get; set; }
+        public double PoissonRatio { get; set; }
+
+        /// <summary>
+        /// Получение расчетного сопротивления грунта
+        /// для 1-й и 2-й группы предельных состояний
+        /// </summary>
+        /// <returns></returns>
+        public abstract double[] GetSoilResistance();
+    }
+}
---
+++ BLL/Entity/Soil/SoilParameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDBLL.Entity.Soil
+{
+    public class SoilParameter
+    {
+        public int Id;
+        public SoilParameterKind SoilParameterKind;
+        public double CrcValue;
+        public double FirstDesValue;
+        public double ScndDesValue;
+    }
+}
