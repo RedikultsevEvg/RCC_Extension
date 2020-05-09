6fcc8cd288d47402ec8ae389dc75200bff6e67d8
Evgeny Redikultsev <effect-project@yandex.ru>
2020-05-09 17:21:13 +0500

    Добавил оффсеты для стальной базы

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.02.24.1")]
-[assembly: AssemblyFileVersion("2020.02.24.1")]
+[assembly: AssemblyVersion("2020.05.09.1")]
+[assembly: AssemblyFileVersion("2020.05.09.1")]
